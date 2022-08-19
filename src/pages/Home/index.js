78e16434aaff3ba1812909f53d97f861a4704408
LCampi01 <lucas.ezequiel001@gmail.com>
2022-08-19 09:46:00 -0300

    Create details card

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { Container } from "@mui/material";
+import DetailsCard from "../../components/DetailsCard";
 
 const Home = () => {
-  return <div>Home</div>;
+  const vectors = () => {
+    return (
+      <div>
+        <div className="vectorTwo" />
+        <div className="vectorEight" />
+        <div className="vectorTen" />
+        <div className="vectorSeven" />
+      </div>
+    );
+  };
+
+  return (
+    <Container maxWidth="false" sx={{ fontFamily: "Satoshi" }}>
+      <DetailsCard />
+      {vectors()}
+    </Container>
+  );
 };
 
 export default Home;
