e3905ebdd4233664a914f06c3beac53b597e1fd2
Quimey Marquez <lucas@code-care.pro>
2022-08-25 11:42:44 -0300

    Feat Allow redirect on thanks dialog

--- src/components/MyDialog/DialogSomethingWrong.js
+++ src/components/MyDialog/DialogSomethingWrong.js
@@ -1,13 +1,12 @@
 import React from "react";
 import MyDialog from ".";
 
-const DialogSomethingWrong = ({ open, onSubmit }) => {
+const DialogSomethingWrong = ({ open }) => {
   return (
     <MyDialog
       title='Oops! Something wrong :('
       text={"You misspelled the form.\nPay attention to the selected fields and fill in the information correctly. And you can continue shopping"}
       buttonText='Again'
-      onSubmit={onSubmit}
       open={open} />
   );
 };
